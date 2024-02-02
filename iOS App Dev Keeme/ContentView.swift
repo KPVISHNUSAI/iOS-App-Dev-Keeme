4dc610cfec1d7a4bf386fedb5d3276b47aa13eca
KPVISHNUSAI <kpvishnusai1111@gmail.com>
2024-02-02 09:48:08 +0530

    Initial Commit

---
+++ iOS App Dev Keeme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS App Dev Keeme
+//
+//  Created by user2 on 02/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS App Dev Keeme/iOS_App_Dev_KeemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_Dev_KeemeApp.swift
+//  iOS App Dev Keeme
+//
+//  Created by user2 on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_App_Dev_KeemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
